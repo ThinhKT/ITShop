7c30be36389887bb6882c373c1f0e73e927f3470
quocitspkt <quocitspkt@gmail.com>
2020-07-08 14:32:40 +0700

    Tìm kiếm có gợi ý

---
+++ WEB/Dao/ProductDao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB.Dao
+{
+    public class ProductDao
+    {
+        ShopEntities db = null;
+        public ProductDao()
+        {
+            db = new ShopEntities();
+        }
+
+        
+        public List<string> ListName(string keyword)
+        {
+            return db.Products.Where(x => x.Name.Contains(keyword)).Select(x => x.Name).ToList();
+        }
+        
+        
+    }
+}
